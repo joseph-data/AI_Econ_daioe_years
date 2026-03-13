be6b0de7403f33b54ad2115d6426032181381eef
joseph-data <oluochjoseph00@gmail.com>
2026-03-13 01:40:30 +0100

    added the logos

--- app_version2.py
+++ app_version2.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 from shiny import reactive
 from shiny.express import input, render, ui
 from shinywidgets import render_widget
+import datetime
 
 from setup import (
     AGES,
@@ -24,8 +25,45 @@ from setup import (
     first_cols,
 )
 
+### ------------ DATA SETUP --------------- ###
+
+CHOICES_BY_LEVEL = build_choices_by_level(lf, LEVELS)
+@reactive.effect
+def _():
+    ui.update_selectize(
+        "occupation_search",
+        choices=CHOICES_BY_LEVEL[input.level()],
+        selected=[],
+        server=True,   # recommended if lots of occupations
+    )
+
+@reactive.calc
+def q_base() -> pl.LazyFrame:
+    metric = input.metric()
+    yr_min, yr_max = input.year_range()
+    q = lf.filter(
+        (pl.col("level") == input.level()) &
+        (pl.col("year").is_between(yr_min, yr_max)) &
+        (pl.col("sex").is_in(input.sex_search())) &
+        (pl.col("age_group").is_in(input.age_search()))
+        ).select(
+        pl.col(first_cols),
+        pl.col(f"^(pctl_)?{metric}.*$")
+    )
+    return q.cache()  
+
+@reactive.calc
+def filtered_lf() -> pl.LazyFrame:
+    occ = input.occupation_search()
+    q = q_base()
+
+    if occ:
+        q = q.filter(pl.col("occupation").is_in(occ))
+
+    return q
 
 
+### ------------ APP UI SETUP --------------- ###
 
 ui.page_opts(
     title="Yearly AI Exposure Dashboard",
@@ -35,13 +73,13 @@ ui.page_opts(
     full_width=True,
 )
 
-with ui.navset_pill_list(id="tab", widths=(2,8)):
+with ui.navset_pill_list(id="tab", widths=(1,11)):
     with ui.nav_panel("Occupation View", value="occupations"):
-        with ui.card(height="1000px"):  
+        with ui.card():  
             #ui.card_header("Changes in Employment by Occupation")
 
             with ui.layout_sidebar():  
-                with ui.sidebar(bg="#00000000", width=200, open="desktop"):  
+                with ui.sidebar(bg="#00000000", width=220, open="desktop"):  
                     ui.input_select(
                         "level", 
                         "Level 🇸🇪", 
@@ -56,11 +94,14 @@ with ui.navset_pill_list(id="tab", widths=(2,8)):
                         selected=[],
                         multiple=False,
                         options={
-                            "placeholder": "Software Developers...",
+                            "placeholder": "Accountants ...",
                             "create": False,
                             "plugins": ["clear_button"],
                         },
                     )  
+
+
+
 
                 ui.markdown("""
     ##  Changes in Employment by Occupation
@@ -151,42 +192,13 @@ with ui.navset_pill_list(id="tab", widths=(2,8)):
             @render.data_frame
             def table_new():
                 return filtered_lf().head(20).collect().to_pandas()
+                    
+        # @render.text
+        # def current_time():
+        #     reactive.invalidate_later(1)
+        #     return datetime.datetime.now().strftime("%H:%M:%S %p")
 
 
 
-CHOICES_BY_LEVEL = build_choices_by_level(lf, LEVELS)
-@reactive.effect
-def _():
-    ui.update_selectize(
-        "occupation_search",
-        choices=CHOICES_BY_LEVEL[input.level()],
-        selected=[],
-        server=True,   # recommended if lots of occupations
-    )
-
-@reactive.calc
-def q_base() -> pl.LazyFrame:
-    metric = input.metric()
-    yr_min, yr_max = input.year_range()
-    q = lf.filter(
-        (pl.col("level") == input.level()) &
-        (pl.col("year").is_between(yr_min, yr_max)) &
-        (pl.col("sex").is_in(input.sex_search())) &
-        (pl.col("age_group").is_in(input.age_search()))
-        ).select(
-        pl.col(first_cols),
-        pl.col(f"^(pctl_)?{metric}.*$")
-    )
-    return q.cache()  
-
-@reactive.calc
-def filtered_lf() -> pl.LazyFrame:
-    occ = input.occupation_search()
-    q = q_base()
-
-    if occ:
-        q = q.filter(pl.col("occupation").is_in(occ))
-
-    return q
 
 
